69a69f4dd224fc2f588d32c8697a7f185d8d2475
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-04-20 22:20:14 +0530

    Deletion in linked list

---
+++ Deletion in linked list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+display(struct node *head)
+{
+    if(head == NULL)
+    {
+        printf("NULL\n");
+    }
+    else
+    {
+        printf("%d\n", head -> data);
+        display(head->next);
+    }
+}
